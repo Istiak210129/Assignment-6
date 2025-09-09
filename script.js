a233c498914eccec28ecf86266591704ddc09619
Istiak <istiakuddin929@gmail.com>
2025-09-09 23:19:09 +0600

    finished

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-
+let total = 0;
+  let ammount = document.getElementById("value");
 let loadCategory = ()=>{
+
     let url = "https://openapi.programming-hero.com/api/categories";
 
     fetch(url)
@@ -19,14 +21,14 @@ let displayCategory =(loads)=>{
         let catagorieDiv = document.createElement("div");
 
         catagorieDiv.innerHTML = `
-           <button id="active-btn-${load.id}" onclick="displayCat(${load.id})" class="btn w-full hover:bg-[#15803D] category-btn">${load.category_name}</button>
+           <button id="active-btn-${load.id}"  onclick="displayCat(${load.id}); manageSpin(${true});" class="btn w-full hover:text-white hover:bg-[#15803D] category-btn">${load.category_name}</button>
         `
 
         categorieContainer.appendChild(catagorieDiv);
     })
 }
 
-
+// remove btn
 let removeActive = () =>{
   let cateBtn = document.querySelectorAll(".category-btn");
 
@@ -62,18 +64,20 @@ let displayCat = (id) =>{
 
         plantMain.innerHTML = `
          <div class="plants-card shadow-xl rounded-md p-4  h-full  ">
-          <img src="${data.image}"  class="w-full h-[15em] border-2 rounded-md bg-contain" alt="">
+          <img src="${data.image}"  class="w-full h-[15em]  rounded-md bg-contain" alt="">
           <h1 class=" btn border-none font-bold my-2">${data.name}</h1>
           <p class="text-gray-500 h-[10em] overflow-auto my-4">${data.description}</p>
           <div class="flex justify-between items-center my-4" >
             <button class="btn bg-[#CFF0DC] rounded-xl text-[#15803D]">${data.category}</button>
             <p class="font-semibold"> ৳${data.price}</p>
           </div>
-          <button class="btn mt-2 w-full bg-[#15803D]">Add to Cart</button>
+          <button class="btn mt-2 w-full text-white bg-[#15803D]">Add to Cart</button>
         </div>
         `
         cardContainer.appendChild(plantMain);
     })
+
+    manageSpin(false);
 
     
  }
@@ -82,6 +86,8 @@ let displayCat = (id) =>{
 // main card category
 
  let  mainCard = ()=> {
+
+  // manageSpin(true);
     let url2 = "https://openapi.programming-hero.com/api/plants";
 
     fetch(url2)
@@ -89,8 +95,25 @@ let displayCat = (id) =>{
     .then((data2) => displayCard(data2.plants));
  }
 
+// manage spinner
+
+  let manageSpin = (status) => {
+    if(status == true)
+    {
+      document.getElementById("spin").classList.remove("hidden");
+      document.getElementById("main-card").classList.add("hidden");
+    }else{
+       document.getElementById("main-card").classList.remove("hidden");
+       document.getElementById("spin").classList.add("hidden");
+     
+
+    }
+  }
+
+
 
  let displayCard = (plantstree) =>{
+   
    
     // console.log(plantstree);
     let cardContainer = document.getElementById("main-card");
@@ -102,23 +125,76 @@ let displayCat = (id) =>{
         plantDiv.innerHTML = `
               
         <div class="plants-card shadow-xl rounded-md p-4  h-full  ">
-          <img src="${plant.image}"  class="w-full h-[15em] border-2 rounded-md bg-contain" alt="">
+          <img src="${plant.image}"  class="w-full h-[15em]  rounded-md bg-contain" alt="">
           <h1 onclick="loadDetail(${plant.id})"  class=" btn border-none font-bold my-2">${plant.name}</h1>
           <p class="text-gray-500 h-[10em] overflow-auto my-4">${plant.description}</p>
           <div class="flex justify-between items-center my-4" >
             <button class="btn bg-[#CFF0DC] rounded-xl text-[#15803D]">${plant.category}</button>
             <p class="font-semibold"> ৳${plant.price}</p>
           </div>
-          <button class="btn mt-2 w-full bg-[#15803D]">Add to Cart</button>
+          <button onclick="addCart(${plant.id})" class="btn mt-2 w-full bg-[#15803D] text-white">Add to Cart</button>
         </div>
         
         `
         cardContainer.appendChild(plantDiv);
-    })
+    });
+
+    // manageSpin(false);
+
+  }
+  
+  let addCart = (id) =>{
+    fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
+    .then((res) => res.json())
+    .then((cart => displayCart(cart.plants)));
+   
+  }
+
+ let displayCart = (carts) =>{
+  // console.log(carts.name);
+  let getCart = document.getElementById("add-cart");
 
 
 
- } 
+  total = Number(carts.price) + total;
+
+  ammount.innerText = `৳ ${total}`;
+
+  let newCart = document.createElement("div");
+  newCart.innerHTML = 
+  `
+     <div id="remove-btn-${carts.id}" class="cart-contain flex items-center justify-between  p-3 my-2 rounded-md bg-[#CFF0DC]">
+             <div  class="price">
+              <h1 class="font-bold">${carts.name}</h1>
+              <p class="font-semibold my-2">৳${carts.price}</p>
+            </div>
+            <div>
+              <button onclick="remove(${carts.id},${carts.price})" class="btn bg-[#CFF0DC]"><i class="fa-solid fa-xmark"></i></button>
+            </div>
+          </div>
+
+  `
+  
+  getCart.appendChild(newCart);
+ }
+
+//  remove btn
+  let remove = (id,price) =>{
+    
+    console.log(price,id);
+    
+    let removeCart = document.getElementById(`remove-btn-${id}`);
+
+    total = total - Number(price);
+
+    ammount.innerText = `৳${total}`;
+    
+     removeCart.style.display = "none";
+
+  }
+
+
+
 
  let loadDetail = (id) =>{
   let url4 = `https://openapi.programming-hero.com/api/plant/${id}`
@@ -137,7 +213,7 @@ let displayCat = (id) =>{
     `
      
         <h1 class="font-bold">${card.name}</h1>
-        <img src="${card.image}" class="w-full h-[15em] mx-auto  my-2 rounded-md bg-contain border-2" alt="">
+        <img src="${card.image}" class="w-full h-[15em] mx-auto  my-2 rounded-md bg-contain " alt="">
         <h1 class="font-semibold">${card.category}</h1>
         <p class="my-2">${card.description}</p>
    `
@@ -154,43 +230,3 @@ mainCard();
 
 
 
-/*category
-: 
-"Fruit Tree"
-description
-: 
-"A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals."
-id
-: 
-1
-image
-: 
-"https://i.ibb.co.com/cSQdg7tf/mango-min.jpg"
-name
-: 
-"Mango Tree"
-price
-: 
-500 */
-
-
-
-/*
-message
-: 
-"successfully fetched plants data filtered by category"
-plants
-: 
-Array(3)
-0
-: 
-{id: 13, image: 'https://i.ibb.co.com/391CtLWD/teak-min.jpg', name: 'Teak', description: 'A high-value hardwood tree known for its durabilit…Widely used in luxury furniture and shipbuilding.', category: 'Timber Tree', …}
-1
-: 
-{id: 14, image: 'https://i.ibb.co.com/QvGgW8Fb/mahogony-min.jpg', name: 'Mahogany', description: 'A premium timber tree producing richly colored, st…Favored for fine furniture and interior paneling.', category: 'Timber Tree', …}
-2
-: 
-{id: 15, image: 'https://i.ibb.co.com/Lz6BSq4Z/sal-min.jpg', name: 'Sal Tree', description: 'A tall hardwood tree producing extremely strong ti…ively in heavy construction and railway sleepers.', category: 'Timber Tree', …}
-length
-: 
-*/
